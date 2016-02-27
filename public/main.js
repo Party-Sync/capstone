81ee564b5aacef15cedecf5ce0dea863222d971b
Luke Antonelli <luke.antonelli@gmail.com>
2016-02-27 13:49:03 -0600

    Added Modal to index.html and main.js

--- public/main.js
+++ public/main.js
@@ -1,1 +1,39 @@
-main.js
+// Social Account User Authentication 
+
+	var myFirebaseRef = new Firebase("https://partysync2.firebaseio.com/");
+
+	$('.btn-facebook').click(function(){
+	var ref = new Firebase("https://partysync2.firebaseio.com");
+	ref.authWithOAuthPopup("facebook", function(error, authData) {
+	  if (error) {
+	    console.log("Login Failed!", error);
+	  } else {
+	    console.log("Authenticated successfully with payload:", authData);
+	    $('body').append("<img src='"+ authData.facebook.profileImageURL + "'>");
+	    // $('.modal-dialog');  ---Need to find a way to dismiss the modal here.
+	  }
+	});
+	});
+
+	$('.btn-twitter').click(function(){
+		var ref = new Firebase("https://partysync2.firebaseio.com");
+	ref.authWithOAuthPopup("twitter", function(error, authData) {
+	  if (error) {
+	    console.log("Login Failed!", error);
+	  } else {
+	    console.log("Authenticated successfully with payload:", authData);
+
+	  }
+	});
+	});
+
+	$('.btn-google').click(function(){
+		var ref = new Firebase("https://partysync2.firebaseio.com");
+		ref.authWithOAuthPopup("google", function(error, authData) {
+	  if (error) {
+	    console.log("Login Failed!", error);
+	  } else {
+	    console.log("Authenticated successfully with payload:", authData);
+	  }
+	});
+	});
