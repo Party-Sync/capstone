71ea4dd871f68b96d9d100c69bea98104b2f43f7
Raymond Kim <Ray@Rays-MacBook-Pro.local>
2016-02-29 18:33:14 -0600

    FINALLY

--- public/partysync/indexNew.js
+++ public/partysync/indexNew.js
@@ -150,4 +150,47 @@ $(document).ready(function() {
 			});
 		});
 	});
+// Social Account User Authentication 
+
+var myFirebaseRef = new Firebase("https://partysync2.firebaseio.com/");
+
+	$('.btn-facebook').click(function(){
+	var ref = new Firebase("https://partysync2.firebaseio.com");
+	ref.authWithOAuthPopup("facebook", function(error, authData) {
+	  if (error) {
+	    console.log("Login Failed!", error);
+	  } else {
+	    console.log("Authenticated successfully with payload:", authData);
+	    $('.form-container').css('display', 'inherit');
+	    $('.form-container').append("<img src='"+ authData.facebook.profileImageURL + "'>");
+	    // $('.modal-dialog');  ---Need to find a way to dismiss the modal here.
+	  }
+	});
+	});
+
+	$('.btn-twitter').click(function(){
+		var ref = new Firebase("https://partysync2.firebaseio.com");
+	ref.authWithOAuthPopup("twitter", function(error, authData) {
+	  if (error) {
+	    console.log("Login Failed!", error);
+	  } else {
+	    console.log("Authenticated successfully with payload:", authData);
+
+	  }
+	});
+	});
+
+	$('.btn-google').click(function(){
+		var ref = new Firebase("https://partysync2.firebaseio.com");
+		ref.authWithOAuthPopup("google", function(error, authData) {
+	  if (error) {
+	    console.log("Login Failed!", error);
+	  } else {
+	    console.log("Authenticated successfully with payload:", authData);
+	  }
+	});
+	});
+	
 }); // END OF $(document).ready().
+
+
